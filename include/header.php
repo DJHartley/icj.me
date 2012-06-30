04239fb75bf17dfde0f799dca826f3f4e6417952
Callum Jones <cj@icj.me>
2012-06-30 18:58:58 +0100

    general browser fixes

--- include/header.php
+++ include/header.php
@@ -24,18 +24,35 @@ if(!$nocrawl) {
 <!-- styles -->
 <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico">
 <link rel="apple-touch-icon" href="./images/icon_ios.png">
-<link rel="stylesheet" type="text/css" href="./css/style.min.css?v=3">
+<link rel="stylesheet" type="text/css" href="./css/style.min.css">
+<?php
+	if($extra_css) {
+		echo '<style type="text/css">';
+		echo $extra_css;
+		echo '</style>';
+	}
+?>
 
 <!-- scripts -->
 <?php 
-	// iOS formatting - including orientation change
-	if(strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') || strstr($_SERVER['HTTP_USER_AGENT'],'iPod')) {
-		echo '<meta name="viewport" content="width=device-width, initial-scale=0.533, maximum-scale=0.533, user-scalable=no">';
-		echo '</head>
-<body onorientationchange="updateOrientation();">';	
+	// mobile formatting - including orientation change
+	$supported_browsers = array('iPhone', 'iPod', 'Android');
+	foreach ($supported_browsers as $browser) {
+		if( strstr($_SERVER['HTTP_USER_AGENT'], $browser)) {
+			$mobile = true;
+		}	
+	}
+	if ($mobile) {
+		echo '<meta name="viewport" content="width=device-width, initial-scale=0.533">';
+		echo '<script type="text/javascript" src="/assets/js/iosOrientation.js"></script>';
+		echo '</head><body onorientationchange="updateOrientation();">';	
 	} else {
-		echo '</head>
-<body>';
+		echo '</head><body>';
+	}
+
+	if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')) {
+		// this is internet explorer, cry.
+		echo '<div id="iebanner">Error: The way that your browser <i>doesn\'t work</i> is unpretty-ing my website. Please <a href="http://www.google.com/chrome">download a respectable browser</a>.</div>';
 	}
 ?>
 
