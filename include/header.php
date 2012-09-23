e54ae54fb55d7f04cae4858a112d77d5ac9a1181
Callum Jones <cj@icj.me>
2012-09-23 11:29:25 +0100

    update header.php to a somewhat messier but more useful version

--- include/header.php
+++ include/header.php
@@ -1,87 +1,145 @@
-<!-- Header file -->
-<!-- Meta tags -->
+<?php
+	// mobile formatting - including orientation change
+	$supported_browsers = array('iPhone', 'iPod', 'Android');
+	foreach($supported_browsers as $browser) {
+		if(strstr($_SERVER['HTTP_USER_AGENT'], $browser)) {
+			$mobile = true;
+		}
+	}
+	
+	// non displaying php that has uses in other files
+	$country_code = geoip_country_code_by_name($_SERVER["REMOTE_ADDR"]);
+?>
+<!-- header file -->
+<!-- meta tags -->
 <meta name="googlebot" content="NOODP, nofollow">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<meta name="format-detection" content="telephone=no">
-
+<meta name="apple-mobile-web-app-title" content="icj.me">
 <?php 
+
+if($title && $mobile && $index_page) {
+	echo "<title>icj.me</title>";
+} else {
+	echo "<title>$title</title>";
+}
+
+
 if(!$nocrawl) {
-// basic meta info
+	// basic meta info
 	if(!$description) {
-		$description = "Just a Penguin is the home of cj, an aspiring software developer.";
+		$description = "Just a Penguin is the website of cj. Here you can find iOS firmware links, tutorials and other stuffs.";
 	}
-	echo '<meta name="description" content="'.$description.'"/>' . "\n";
+	echo '<meta name="description" content="' . $description . '"/>' . "\n";
 
 	if (!$keywords) {
 		$keywords = "icj, jailbreak, cj, ikeyhelper, ios firmware links, download ios, ios, iphone, ipad";
 	}
-	echo '<meta name="keywords" content="'.$keywords.'"/>' . "\n";
+	echo '<meta name="keywords" content="' . $keywords . '"/>' . "\n";
 } else {
 	echo '<meta name="robots" content="noindex" />';
 }
+
 ?>
 
 <!-- styles -->
-<link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico">
-<link rel="apple-touch-icon" href="./images/icon_ios.png">
-<link rel="stylesheet" type="text/css" href="./css/style.min.css">
-<?php
+<link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.ico">
+<link rel="apple-touch-icon-precomposed" href="/assets/images/apple-touch-icon-precomposed.png">
+<?php 
+	if(!$index_page || $mobile) { 
+		echo '<link rel="stylesheet" type="text/css" href="/assets/css/style.min.css">';
+	} elseif($index_page) {
+		echo '<link rel="stylesheet" type="text/css" href="/assets/css/index.css">';
+	}
+	
+	// extra css for separate pages
 	if($extra_css) {
 		echo '<style type="text/css">';
 		echo $extra_css;
 		echo '</style>';
+	} 
+	if($extra_css_url) {
+		echo '<link rel="stylesheet" type="text/css" href="' . $extra_css_url . '">';
 	}
-?>
 
-<!-- scripts -->
-<?php 
-	// mobile formatting - including orientation change
-	$supported_browsers = array('iPhone', 'iPod', 'Android');
-	foreach ($supported_browsers as $browser) {
-		if( strstr($_SERVER['HTTP_USER_AGENT'], $browser)) {
-			$mobile = true;
-		}	
-	}
+	echo "\n\n<!-- scripts -->\n";
+	
+	// google analytics
+	include 'analytics.php';
+	
+	$IE = strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE');
+	
 	if ($mobile) {
-		echo '<meta name="viewport" content="width=device-width, initial-scale=0.533">';
-		echo '<script type="text/javascript" src="/assets/js/iosOrientation.js"></script>';
-		echo '</head><body onorientationchange="updateOrientation();">';	
+		// echo '<meta name="apple-mobile-web-app-capable" content="yes" />';
+		// echo '<meta name="apple-mobile-web-app-status-bar-style" content="black" />';
+		echo '<meta name="viewport" content="width=device-width, initial-scale=0.533, maximum-scale=0.533, user-scalable=0"/> ';
+		echo '<script type="text/javascript" src="/assets/js/ios.js"></script>';
+		echo '<script src="/assets/js/addressbar.js" type="text/javascript"></script>';
+		echo '</head>
+		<body onorientationchange="updateOrientation();">';
 	} else {
-		echo '</head><body>';
+		echo "\n</head>\n<body>";
 	}
 
-	if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')) {
+	if($IE && !$index_page) {
 		// this is internet explorer, cry.
-		echo '<div id="iebanner">Error: The way that your browser <i>doesn\'t work</i> is unpretty-ing my website. Please <a href="http://www.google.com/chrome">download a respectable browser</a>.</div>';
+		echo '<div id="iebanner">This site is better displayed on a standards compliant browser. To view this site properly, please <a href="http://www.google.com/chrome">download a respectable browser</a></div>';
 	}
+
+	// display highlighted link on the navbar if this page is the one you are viewing
+	function is_current_page($url, $title, $follow = false) {
+		if (($_SERVER['REQUEST_URI'] == $url) || (strpos($_SERVER['REQUEST_URI'], $url) !== false) && $follow) {
+			return "\t" . '<a class="current" href="' . $url . '">' . $title . "</a>\n";
+		} else {
+			return "\t" . '<a href="' . $url . '">' . $title . "</a>\n";
+		}
+	}
+
+	// navigation
+	$navigation_html .= is_current_page("/", "Home");
+	$navigation_html .= is_current_page("/projects", "Projects", true);
+	$navigation_html .= is_current_page("/ios", "Firmware Links", true);
+	$navigation_html .= is_current_page("/tutorials", "Tutorials", true);
+	if(!$index_page || $mobile) $navigation_html .= is_current_page("/minecraft", "Minecraft", true);
+	$navigation_html .= is_current_page("/contact", "Contact", true);
+
 ?>
+
+
+<?php if(!$index_page || $mobile) { ?>
 
 <div id="header">
 	<div id="center">
-		<a href="/"><img class="logo" src="./images/JustaPenguin.png" alt="Just A Penguin"></a>
+		<a href="/"><img class="logo" src="/assets/images/logo.png" alt="Just A Penguin"></a>
 	</div>
 </div>
 
 <div id="navigation">
-<?php
-	// formatting for the current page
-	// sometimes recursive (e.g. /ios/faq will show "Firmware Links" as the active page)
-	function is_current_page($url, $title, $follow = false) {
-		if (($_SERVER['REQUEST_URI'] == $url) || (strpos($_SERVER['REQUEST_URI'], $url) !== false)  && $follow) {
-			echo '	<a class="current" href="' . $url . '">' . $title . "</a>\n";
-		} else {
-			echo '	<a href="' . $url . '">' . $title . "</a>\n";
-		}
-	}
-
-	// load navigation bar
-	is_current_page("/", "Home");
-	is_current_page("/iKeyHelper", "iKeyHelper");
-	is_current_page("/ios", "Firmware Links", true);
-	is_current_page("/tutorials", "Tutorials", true);
-	is_current_page("/dont-afk-and-minecraft", "Minecraft");
-	is_current_page("/contact", "Contact", true);
-
-?>
+	<?php echo $navigation_html; ?>
 </div>
-<!--/ Header file -->
+
+<?php } elseif($index_page && !$mobile) { ?>
+
+<div id="header">
+	<div id="center">
+		<img id="penguin" src="/assets/images/penguin.png" alt="A pretty picture of a penguin, that you're really missing out on.">
+		<img id="logo" src="/assets/images/logo.png" alt="Just a Penguin">
+		<p>Impersonating penguins since 2010.</p>
+		<div id="navigation">
+			<?php echo $navigation_html; ?>
+		</div>
+	</div>		
+</div>
+
+<?php } ?>
+
+<!--/ header file -->
+<!-- content -->
+
+<?php if($mobile && $index_page) { ?>
+
+<div id="container" class="center">
+	<img class="penguin" src="/assets/images/penguin.png" alt="A pretty picture of a penguin, that you're really missing out on.">
+	<h2>Just a penguin</h2>
+</div>
+
+<?php } ?>
