292a885b090d3675fe4df920da2fb3fae29c683d
Callum Jones <cj@icj.me>
2012-09-23 11:30:02 +0100

    add index.php to go with index.less

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<?php
+	$title = 'Just a Penguin';
+	$index_page = true;
+	include($_SERVER['DOCUMENT_ROOT'] . "/include/header.php");
+	include($_SERVER['DOCUMENT_ROOT'] . "/include/footer.php");
+?>
+</body>
+</html>
